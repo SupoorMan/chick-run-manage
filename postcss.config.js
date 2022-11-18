e1e930d6ca4f80bb3d2d1033b915ebaa8817ff3a
poor <940010773@qq.com>
2022-11-18 15:32:34 +0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "plugins":{
+    "autoprefixer":{
+      overrideBrowserslist:[
+        "Android 4.1",
+        "IOS 7.1",
+        "Chrome >31",
+        "not ie <=11",
+        "> 1%",
+        "last 2 versions"
+      ]
+    },
+    "postcss-pxtorem":{
+      rootValue: 75,
+      unitPrecision: 6,
+      propList: ['*'],
+      replace: true,
+      mediaQuery: true,
+      minPixeValue: 1,
+      exclude: /node_modules/i
+    }
+  }
+}
